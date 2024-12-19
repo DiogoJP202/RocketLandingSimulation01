819d4de2ee77f93e9ab5abfebdc24d376a064d5a
DiogoJP202 <diogojp202@gmail.com>
2024-12-19 01:37:54 -0300

    Outro foguete adicionado.

--- RocketLandingSimulation/Program.cs
+++ RocketLandingSimulation/Program.cs
@@ -26,9 +26,24 @@ class Program
         Console.WriteLine("\n | \n/ \\\n| |\n| |\n|_|\n/ \\");
         Thread.Sleep(100);
         Console.Clear();
-        Console.WriteLine("\n\n | \n/ \\\n| |\n| |\n|_|\n/ \\\n***");
+        Console.WriteLine("\n\n | \n/ \\\n| |\n| |\n|_|\n/ \\\n***\n");
 
         Console.WriteLine("*** The rocket has landed. Woohoo! Another successful landing! :3 ***");
+        Thread.Sleep(5000);
+        Console.Clear();
+
+        string rocket = "    \n     | \n    / \\\n    | |\n    | |\n    |_|\n    / \\";
+
+        for (int i = 10; i >= 0; i--)
+        {
+            Console.Clear();
+            Console.WriteLine(rocket);
+            rocket = "\n" + rocket;
+            Thread.Sleep(500);
+        }
+        Console.WriteLine("    ***\n");
+        Console.WriteLine("***The rocket has landed agaiiin! Another successful landing! :3 ***");
+
         Console.ReadKey();
     }
 }
