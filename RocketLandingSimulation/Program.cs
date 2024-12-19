edbf57001e2f90c1e0fd90fc7a574049804341e3
DiogoJP202 <diogojp202@gmail.com>
2024-12-19 01:28:29 -0300

    Add project files.

---
+++ RocketLandingSimulation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread.Sleep(500);
+        Console.WriteLine("/ \\");
+        Thread.Sleep(500);
+        Console.Clear();
+        Console.WriteLine("|_|\n/ \\");
+        Thread.Sleep(500);
+        Console.Clear();
+        Console.WriteLine("| |\n|_|\n/ \\");
+        Thread.Sleep(500);
+        Console.Clear();
+        Console.WriteLine("| |\n| |\n|_|\n/ \\");
+        Thread.Sleep(500);
+        Console.Clear();
+        Console.WriteLine("/ \\\n| |\n| |\n|_|\n/ \\");
+        Thread.Sleep(500);
+        Console.Clear();
+        Console.WriteLine(" | \n/ \\\n| |\n| |\n|_|\n/ \\");
+        Thread.Sleep(500);
+        Console.Clear();
+        Console.WriteLine("\n | \n/ \\\n| |\n| |\n|_|\n/ \\");
+        Thread.Sleep(100);
+        Console.Clear();
+        Console.WriteLine("\n\n | \n/ \\\n| |\n| |\n|_|\n/ \\\n***");
+
+        Console.WriteLine("*** The rocket has landed. Woohoo! Another successful landing! :3 ***");
+        Console.ReadKey();
+    }
+}
